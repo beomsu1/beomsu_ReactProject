a75537a6d44d3ccef2739e2016209c26b75ccee6
Beomsu <beumsu1221@gmail.com>
2023-07-19 13:13:04 +0900

    로그인 state 값 변경

--- src/reducers/loginSlice.js
+++ src/reducers/loginSlice.js
@@ -59,12 +59,17 @@ const loginSlice = createSlice({
 
       if(errorMsg){
         state.errorMsg = errorMsg
+        return
       }
 
-      state.loading = false
-      state.email = email
-      state.nickname=nickname
-      state.admin = admin
+      // state.loading = false
+      // state.email = email
+      // state.nickname=nickname
+      // state.admin = admin
+      // 이것들을 줄인코드 ->
+
+      // action.payload - 진짜 데이터 (토큰값도 포함되어있음)
+      state = action.payload
 
       setCookie("login", JSON.stringify(action.payload), 1)
     })
